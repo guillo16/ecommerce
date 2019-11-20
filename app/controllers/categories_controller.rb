f41046e01ac2911c6905b906f487ef38b6a7cea8
guillermo varela <guillovarela16@gmail.com>
2019-11-20 12:55:24 -0300

    homepage in progress

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,11 @@
 class CategoriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_category, only: [:show, :destroy]
+
+  def index
+    @categories = Category.all
+    @products = Product.all
+  end
 
   def show
     @categories = Category.all
