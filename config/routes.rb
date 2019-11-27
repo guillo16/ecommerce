5f56aaa645d92f0ed168a6d5df2d544bdfeb028f
guillermo varela <guillovarela16@gmail.com>
2019-11-27 12:05:47 -0300

    reviews added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :categories, only: [:show, :new, :create, :destroy] do
     resources :products, only: [:create]
   end
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resources :reviews, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
