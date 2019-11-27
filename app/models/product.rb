79d46929552d960e351ba34d7bfbe318b05101e8
guillermo varela <guillovarela16@gmail.com>
2019-11-27 11:13:27 -0300

    finish review model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category, dependent: :destroy
   belongs_to :user
-
+  has_many :reviews, dependent: :destroy
   validates :name, presence: true
 end
