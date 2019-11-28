695aaf91a33abe890c52f8add3b1caa0641b852d
guillermo varela <guillovarela16@gmail.com>
2019-11-28 20:52:49 -0300

    add prices in show and index

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
+  monetize :price_cents
 
   def average
     return self.reviews.map { |r| r.rating }.sum / self.reviews.count if self.reviews.count > 0
