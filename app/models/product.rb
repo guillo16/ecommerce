33d59c7dd89bccd0c473dbc3dad2dcb138061ebd
guillermo varela <guillovarela16@gmail.com>
2019-12-03 21:13:33 -0300

    cart model and line items finished

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,4 +10,13 @@ class Product < ApplicationRecord
     return self.reviews.map { |r| r.rating }.sum / self.reviews.count if self.reviews.count > 0
     0
   end
+
+  private
+
+  def not_refereced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 end
